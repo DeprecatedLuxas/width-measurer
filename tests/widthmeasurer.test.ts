9282cb364f407fbbe2232b020ce65bc3187d7712
DeprecatedLuxas <40726067+DeprecatedLuxas@users.noreply.github.com>
2021-04-14 12:53:29 +0200

    this works now i hope.

--- tests/widthmeasurer.test.ts
+++ tests/widthmeasurer.test.ts
@@ -19,8 +19,10 @@ describe("width creation with and without array", () => {
 
     test("basic latin creation with specific font", async () => {
         await measure.create("Basic Latin", {
-            font: "10px Verdana"
+            // On linux Verdana is called DejaVu Sans Verdana.
+            font: "10px DejaVu Sans Verdana"
         });
-        expect(measure.measure("hello", measure.load("basic-latin-10px-verdana.json"))).toBe("23.84");
+    
+        expect(measure.measure("hello", measure.load("basic-latin-10px-dejavu-sans-verdana.json"))).toBe("23.84");
     });
 });
